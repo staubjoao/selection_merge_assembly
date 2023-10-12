f918d042d7a3fed39b8ce531aa11f8fc31c3d051
Sergio A <sasjsergioalvarezjunior@gmail.com>
2023-10-11 21:57:12 -0300

    Apanhei um bocado aqui
    Algo de errado no eax

--- selection_sort.s
+++ selection_sort.s
@@ -3,6 +3,8 @@
     vetorTxt: .asciz "Vetor: "      # String de formatação para print
     vectorElement: .string "%d, "    # String de formatação para print
     teste: .string "\nmenor: %d, indice: %d\n"    
+    teste3: .string "\nedx: %d"    
+
     teste1: .string "teste!!\n"    
     scanVec: .string "%d"           # String de formatação para scanf
     cont: .int 0                    # Variável pra contar número de elemntos
@@ -60,14 +62,26 @@ findMin:
     je imprimeValores
     addl $4, %edi # posição edi[N] para posição edi[N+1],
     addl $1, %ecx
-    movl %edi, %edx
+    movl (%edi), %edx
+
+    #  Imprime pra comparar
+    pushl %edx
+    pushl %eax
+    pushl $teste3
+    call printf
+    addl $12, %esp
+    # edx é o atual, eax é o menor
     cmp %edx, %eax
     jle atualizaMin
     jmp findMin
+
     
 atualizaMin:
-    movl %edi, %eax
-    addl %ecx, %ebx
+    # tem um menor, atualiza o eax
+    movl (%edi), %eax
+    
+    # Tava addl, pq não movl?
+    movl %ecx, %ebx
     addl $4, %edi
     pushl $teste1
     call printf
